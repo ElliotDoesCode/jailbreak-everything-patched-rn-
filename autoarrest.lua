f351ab3a0bdf97641bda88c6cfb3ba39f9513ed2
ElliotDoesCode <59733209+ElliotDoesCode@users.noreply.github.com>
2021-05-27 22:05:44 -0400

    Update autoarrest.lua

--- autoarrest.lua
+++ autoarrest.lua
@@ -1,1 +1,1 @@
-game:GetService("Players").LocalPlayer:Kick("For your safety, this script has been disabled. Expect the script to be back up by 5/25/21")
+game:GetService("Players").LocalPlayer:Kick("For your safety, this script has been disabled. Expect the script to be back up by June. Sorry")
