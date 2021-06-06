a638ce3ac4b04a0d4b1262de7f8af8eff3cde31f
ElliotDoesCode <59733209+ElliotDoesCode@users.noreply.github.com>
2021-06-06 11:53:37 -0400

    Update autoarrest.lua

--- autoarrest.lua
+++ autoarrest.lua
@@ -7,4 +7,4 @@
         -Patched
 ]]
 
-game.Players.LocalPlayer:Kick("Jailbreak just patched the instant tp, wait while I use the new teleport bypass")
+game.Players.LocalPlayer:Kick("Jailbreak just patched the instant tp. Don't expect this script to be up for a while. I'll release my autorob soon so stay tuned for that")
