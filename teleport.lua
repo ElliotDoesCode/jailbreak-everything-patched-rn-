fedb3bb5e71b16144eeed9e36e10024eb7f2234d
ElliotDoesCode <59733209+ElliotDoesCode@users.noreply.github.com>
2021-06-14 19:37:10 -0400

    Update teleport.lua

--- teleport.lua
+++ teleport.lua
@@ -36,7 +36,7 @@ getgenv().slide = function(cframe, optionalspeed)
         root.CFrame = distancel + distance.Unit * i
         root.Velocity,root.RotVelocity = Vector3.new(),Vector3.new() wait()
         local currentpos = root.Position - Vector3.new(0, root.Position.y, 0) wait() local magafter = ((root.Position - Vector3.new(0,root.Position.y, 0)) - currentpos).magnitude
-        if magafter > 500 then wait(3) slide(cframe, optionalspeed) return end
+        if magafter > 100 then wait(3) slide(cframe, optionalspeed) return end
     end
     game:GetService("Players").LocalPlayer.Character.Humanoid:ChangeState(11)
 end
