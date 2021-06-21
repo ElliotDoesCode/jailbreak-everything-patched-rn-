2b15fe90778dcacacc6b86ed651c1611347e8d1b
ElliotDoesCode <59733209+ElliotDoesCode@users.noreply.github.com>
2021-06-20 23:07:50 -0400

    Update teleport.lua

--- teleport.lua
+++ teleport.lua
@@ -88,7 +88,7 @@ getgenv().slide = function(cframe,...)
 
         move.MaxForce = Vector3.new(9e9,9e9,9e9)
         move.P = 3000
-        move.Velocity = (cframe.p - root.Position).unit * 130
+        move.Velocity = (cframe.p - root.Position).unit * 120
         
         for _,v in pairs(game:GetService("Players").LocalPlayer.Character:GetDescendants()) do
             pcall(function()
